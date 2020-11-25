80d38876d6007261b16cbaa5e4c837126e17e147
Yates <1916963+AlexYates@users.noreply.github.com>
2020-11-25 16:37:11 +0000

    Initial Search history interactions added, persistence of search history TBD.

--- components/Search.vue
+++ components/Search.vue
@@ -2,12 +2,9 @@
   <nav
     id="search"
     aria-live="assertive"
-    class="bg-white flex items-center justify-center"
+    class="bg-white flex flex-col items-center justify-center px-4 pb-2 pt-8"
   >
-    <label
-      class="container flex flex-col mx-auto px-4 py-8 w-full"
-      for="search"
-    >
+    <label class="container flex flex-col mx-auto w-full" for="search">
       <span class="flex mb-2 text-sm">Search product titles</span>
       <div class="flex items-center justify-center">
         <input
@@ -21,7 +18,7 @@
         />
         <Spacer />
         <button
-          class="bg-indigo-700 flex items-center justify-center px-4 py-2 rounded-lg shadow-lg text-white"
+          class="bg-indigo-700 focus:bg-indigo-400 hover:bg-indigo-400 flex items-center justify-center px-4 py-2 rounded-lg shadow-lg text-white"
           @click.prevent="deactivate()"
         >
           <div
@@ -34,15 +31,54 @@
         </button>
       </div>
     </label>
+    <ol class="container flex flex-wrap mx-auto my-2 p-2">
+      <li
+        v-for="term in searchTerms"
+        :key="term.uuid"
+        class="bg-white focus:bg-indigo-400 hover:bg-indigo-400 border flex items-center justify-center mr-2 px-2 py-1 rounded-full focus:text-white hover:text-white text-sm"
+      >
+        <span class="cursor-pointer" @click="searchSet(term.title)">
+          {{ term.title }}
+        </span>
+        <Spacer />
+        <div
+          class="cursor-pointer h-4 w-4"
+          style="fill: currentColor"
+          tabindex="0"
+          @click="remove(term.uuid)"
+          @keypress.enter="remove(term.uuid)"
+          v-html="PhX"
+        />
+      </li>
+    </ol>
   </nav>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import PhMagnifyingGlass from '../phosphor-icons/assets/duotone/magnifying-glass-duotone.svg?raw'
+import PhX from '../phosphor-icons/assets/duotone/x-duotone.svg?raw'
 
 export default {
   name: 'Search',
+  data() {
+    return {
+      searchTerms: [
+        {
+          title: 'exceptur',
+          uuid: 0,
+        },
+        {
+          title: 'anim',
+          uuid: 1,
+        },
+        {
+          title: 'id',
+          uuid: 2,
+        },
+      ],
+    }
+  },
   computed: {
     ...mapGetters('search', {
       searchGet: 'search',
@@ -57,6 +93,9 @@ export default {
     },
     PhMagnifyingGlass() {
       return PhMagnifyingGlass
+    },
+    PhX() {
+      return PhX
     },
   },
   methods: {
@@ -74,6 +113,12 @@ export default {
           title: this.searchGet,
         },
       })
+    },
+    remove(uuid) {
+      const index = this.searchTerms.findIndex((term) => term.uuid === uuid)
+      if (index !== -1) {
+        this.searchTerms.splice(index, 1)
+      }
     },
   },
 }
