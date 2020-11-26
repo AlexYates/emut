745f63522581f3f2784ec5e4b9cc888d830c92e5
Yates <1916963+AlexYates@users.noreply.github.com>
2020-11-26 08:34:16 +0000

    EmptyMessage test.

---
+++ test/components/EmptyMessage.spec.js
@@ -0,0 +1,32 @@
+import { mount /* , shallowMount */ } from '@vue/test-utils'
+
+import EmptyMessage from '@/components/EmptyMessage'
+
+describe('EmptyMessage - html', () => {
+  it('html matches expectations', () => {
+    const line = 'Test'
+    const expectations = `<div class="p-4">
+  <h2 class="mt-16 text-center">
+    ${line}
+    <br>
+    try
+    <routerlink-stub to="/products" class="underline">
+      browsing our products
+    </routerlink-stub> <br>
+    for more inspiration!
+  </h2>
+</div>`
+
+    const wrapper = mount(EmptyMessage, {
+      propsData: {
+        line: line,
+      },
+      stubs: {
+        RouterLink: true,
+      },
+    })
+    const actual = wrapper.html()
+
+    expect(actual).toBe(expectations)
+  })
+})
