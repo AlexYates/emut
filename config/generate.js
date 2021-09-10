6c6486a63398ac0e76fd000a0bef254c8be6694e
Yates <1916963+AlexYates@users.noreply.github.com>
2021-09-10 22:29:03 +0100

    Switched generate from local server to in-memory code.

--- config/generate.js
+++ config/generate.js
@@ -1,20 +1,26 @@
-// import { products } from '../database'
-import axios from 'axios'
+import { products } from '../database'
+// import axios from 'axios'
 
 // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-generate
 const generate = {
   async routes() {
-    const API =
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:5000'
-        : 'https://gentle-stream-10329.herokuapp.com'
-    return await axios.get(`${API}/products`).then(({ data: { products } }) => {
-      return products.map((product) => {
-        return {
-          route: '/product/' + product.uuid,
-          payload: product,
-        }
-      })
+    // const API =
+    //   process.env.NODE_ENV === 'development'
+    //     ? 'http://localhost:5000'
+    //     : 'https://gentle-stream-10329.herokuapp.com'
+    // return await axios.get(`${API}/products`).then(({ data: { products } }) => {
+    //   return products.map((product) => {
+    //     return {
+    //       route: '/product/' + product.uuid,
+    //       payload: product,
+    //     }
+    //   })
+    // })
+    return products.map((product) => {
+      return {
+        route: '/product/' + product.uuid,
+        payload: product,
+      }
     })
   },
 }
