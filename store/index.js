e5fb3d13d4f594a16cdb0e2f12c4eebf88208c2d
Yates <1916963+AlexYates@users.noreply.github.com>
2021-09-11 17:37:48 +0100

    Updated store to use local data.

--- store/index.js
+++ store/index.js
@@ -1,10 +1,12 @@
+import { products } from '../database'
+
 export const actions = {
   async nuxtServerInit({ dispatch }) {
-    const API =
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:5000'
-        : 'https://gentle-stream-10329.herokuapp.com'
-    const { products } = await this.$axios.$get(`${API}/products`)
+    // const API =
+    //   process.env.NODE_ENV === 'development'
+    //     ? 'http://localhost:5000'
+    //     : 'https://gentle-stream-10329.herokuapp.com'
+    // const { products } = await this.$axios.$get(`${API}/products`)
     dispatch('products/load', products)
   },
 }
